f89020aa10ee39487fbdc731bfa6dc548c7830f8
Verma,Shubhaangi <Shubhaangi.Verma@cerner.com>
2021-10-11 16:18:48 +0530

    Addressed review comments

--- Product Management Service/Code/product_management_service.sql
+++ Product Management Service/Code/product_management_service.sql
@@ -2,16 +2,10 @@ CREATE TABLE products (
   id int PRIMARY KEY,
   name varchar(255),
   price decimal(10,2),
-  category varchar(255) FOREIGN KEY REFERENCES category(product_category)
-);
-
-CREATE TABLE product_details (
-  id int PRIMARY KEY,
+  category varchar(255),
   image blob,
-  description text
-);
-
-CREATE TABLE product_category (
-  category varchar PRIMARY KEY,
-  specification JSON 
+  description text,
+  specification JSON,
+  created_on timestamp,
+  last_updated timestamp 
 );
