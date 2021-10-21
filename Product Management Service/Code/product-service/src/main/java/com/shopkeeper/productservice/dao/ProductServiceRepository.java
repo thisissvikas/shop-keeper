0506a0cb0c5260efbef96b97a64ed87424dd0d9d
Verma,Shubhaangi <Shubhaangi.Verma@cerner.com>
2021-10-21 16:44:27 +0530

    created dto and yaml

--- Product Management Service/Code/product-service/src/main/java/com/shopkeeper/productservice/dao/ProductServiceRepository.java
+++ Product Management Service/Code/product-service/src/main/java/com/shopkeeper/productservice/dao/ProductServiceRepository.java
@@ -2,7 +2,7 @@ package com.shopkeeper.productservice.dao;
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.shopkeeper.productservice.model.Product;
+import com.shopkeeper.productservice.dto.Product;
 
 public interface ProductServiceRepository extends CrudRepository<Product, Integer>{
 	
---
+++ Product Management Service/Code/product-service/src/main/java/com/shopkeeper/productservice/dto/Product.java
@@ -0,0 +1,38 @@
+package com.shopkeeper.productservice.dto;
+
+import java.util.Date;
+import java.util.HashMap;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+  @Getter
+  @Setter
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Integer id;
+
+  @Getter @Setter private String name;
+
+  @Getter @Setter private float price;
+
+  @Getter @Setter private String category;
+
+  @Getter @Setter private String description;
+
+  @Getter @Setter private HashMap<String, String> specifications;
+
+  @Getter @Setter private Date createdTimestamp;
+
+  @Getter @Setter private Date updatedTimestamp;
+}
